93a50d5fc3c44e8b7a8f68aef2c3782f976e090b
neutralchaos18 <89124122+neutralchaos18@users.noreply.github.com>
2023-09-23 19:02:02 +1000

    Added some code to Arduino Debug
    Added to program/application. There is an error with the program where it will not run any code in the switch statement after the first iteration. Currently the code is just integrating the pressure sensor, no buttons or LEDs.

--- 90-Arduino-Dev-Configuration/main/main.ino
+++ 90-Arduino-Dev-Configuration/main/main.ino
@@ -13,6 +13,8 @@
 #define BMP_MOSI (11)
 #define BMP_CS   (10)
 
+#define SEA_LEVEL_hPa (float)1013.25 // adjust to local forecast
+
 Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
@@ -20,9 +22,13 @@ Adafruit_BMP280 bmp; // I2C
 void Init_Serial(void);
 bool Init_Pressure_Sensor(void);
 void Configure_Pressure_Sensor(void);
+void Print_Pressure_Sensor_Data(float pressure, float temperature, float altitude);
+void Print_Sensor_Error(void);
+void Print_Sensor_Conformation(void);
 
 // state variable - used to keep track of program state
-enum states_e {INIT, WAIT_FOR_INPUT, RECORD_DATA}
+enum states_e {INIT = 'A', WAIT_FOR_INPUT = 'B', RECORD_DATA = 'C'};
+enum states_e state = INIT;
 
 void setup() {
   // initialise the microcontroller peripherals
@@ -31,24 +37,36 @@ void setup() {
   // initialise serial port
   Init_Serial();
 
-  enum states_e state = INIT;
+  // initialise LED (debugging purposes)
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // debug message
+  Serial.println("Setup complete. Entering main loop.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // wait (approximately) 2 seconds between each loop  
+  delay(2000);
+  Serial.print("Loop. State = ");
+  Serial.println((char)state);
 
+  // state machine
   switch(state) {
-    default: break;
-      case INIT:
+    case INIT:
+        // sensor status
+        bool sensorInitStatus = false;
+
         // try to initialise sensor
-        bool pressureSensorStatus = Init_Pressure_Sensor();
+        sensorInitStatus = Init_Pressure_Sensor();
 
         // if sensor is not found, print error message and break early
-        if (!pressureSensorStatus) {
-          Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-          Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+        if (!sensorInitStatus) {
+          Print_Sensor_Error();
+
           break;
+        }
+        else {
+          Print_Sensor_Conformation();
         }
         
         // apply settings to sensor
@@ -58,12 +76,50 @@ void loop() {
         state = WAIT_FOR_INPUT;
 
         break;
-      case WAIT_FOR_INPUT:
-        // do waiting stuff
+    case WAIT_FOR_INPUT:
+      // do waiting stuff
+
+      // transition to next state
+      state = RECORD_DATA;
+      
+      break;
+    case RECORD_DATA:
+      // data
+      float pressure = 0;
+      float temperature = 0;
+      float altitude = 0;
+      // sensor status
+      bool sensorDataStatus = false;
+
+      // take readings
+      temperature = bmp.readTemperature();
+      pressure = bmp.readPressure();
+      altitude = bmp.readAltitude(SEA_LEVEL_hPa);
+
+      // do some range checking here based on sensor specs here
+      // right now we are just assuming theres no issues
+      sensorDataStatus = true;
+
+      // if there were an issue with the sensor:
+      // print error message and return to INIT state
+      // exit early
+      if (!sensorDataStatus) {
+        Print_Sensor_Error();
+        state = INIT;
+
         break;
-      case RECORD_DATA:
-        // do recording stuff
-        break;
+      }
+
+      // do something with data
+
+      // print data to serial port
+      Print_Pressure_Sensor_Data(pressure, temperature, altitude);
+
+      break;
+    default:
+      state = INIT;
+
+      break;
   }
 }
 
@@ -73,7 +129,10 @@ void loop() {
 void Init_Serial(void) {
    Serial.begin(9600);
   while ( !Serial ) delay(100);   // wait for native usb
-  (void)Serial.println(F("BMP280 test"));
+  (void)Serial.println();
+  (void)Serial.println(F("Serial port active"));
+
+  return;
 }
 
 // initialise the pressure sensor
@@ -97,4 +156,43 @@ void Configure_Pressure_Sensor(void) {
                 Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                 Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                 Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  
+  return;
+}
+
+// print error message about pressure sensor
+// takes: nothing
+// returns: nothign
+void Print_Sensor_Error(void) {
+  (void)Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+              "try a different address!"));
+  (void)Serial.print("SensorID was: 0x");
+  (void)Serial.println(bmp.sensorID(),16);
+
+  return;
+}
+
+// print error (positive) message about pressure sensor
+// takes: nothing
+// returns: nothing
+void Print_Sensor_Conformation(void) {
+  (void)Serial.print("Found valid BMP280 sensor with SensorID: 0x");
+  (void)Serial.println(bmp.sensorID(),16);
+
+  return;
+}
+
+// print pressure sensor data
+// PRESSURE (Pascals) | TEMPERATURE (degrees C) | ALTITUDE (m)
+// takes: pressure, temp, altitude data (as float)
+// returns: nothing
+void Print_Pressure_Sensor_Data(float pressure, float temperature, float altitude) {
+  (void)Serial.print(pressure);
+  (void)Serial.print(" Pa |");
+  (void)Serial.print(temperature);
+  (void)Serial.print(" *C |");
+  (void)Serial.print(altitude);
+  (void)Serial.print(" m");
+
+  return;
 }
